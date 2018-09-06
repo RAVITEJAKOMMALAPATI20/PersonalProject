3791948fccec80cb5eb5d0ce9bdbabe87662aa6e
ravitejakommalapati <ravitejakommalapati@10.250.190.90>
2018-09-06 00:09:16 -0700

    Adding test case java files

---
+++ lab0/src/test/java/com/project/beans/GreetTest.java
@@ -0,0 +1,14 @@
+package com.project.beans;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GreetTest {
+
+	@Test
+	void testGetGreeting() {
+		fail("Not yet implemented");
+	}
+
+}
